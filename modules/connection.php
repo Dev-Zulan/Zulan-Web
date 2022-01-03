a04932a295e43f3aa179e80e93e4717d3233127e
dev-zulan <dev.zulan@gmail.com>
2022-01-03 14:38:15 +0800

    debug attempt 4

--- modules/connection.php
+++ modules/connection.php
@@ -5,17 +5,17 @@ $SQL_USER = getenv('SQL_USER');
 $SQL_PASSWORD = getenv('SQL_PASSWORD');
 $SQL_DATABASE = getenv('SQL_DATABASE');
 
-if($SQL_Handle = new mysqli($SQL_HOST, $SQL_USER, $SQL_PASSWORD, $SQL_DATABASE)) {
-    echo 'success';
-} else {
-    $error = $SQL_Handle->errno . ' ' . $SQL_Handle->error;
-    echo $error;
-}
+$SQL_Handle = new mysqli($SQL_HOST, $SQL_USER, $SQL_PASSWORD, $SQL_DATABASE);
 
 if($SQL_Handle->connect_error) {
     echo '<script>console.log("Error connecting to the database: ")</script>';
     die("Error connecting to the database: " . $SQL_Handle->connect_error);
     return;
 } else {
+    echo $SQL_HOST . '<br />';
+    echo $SQL_USER . '<br />';
+    echo $SQL_PASSWORD . '<br />';
+    echo $SQL_DATABASE . '<br />';
+
     echo '<script>console.log("Connected to MySQL Database!")</script>';
 }
